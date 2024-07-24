4c212952694d71e129da8e63d0b41fa9cf339e96
Rovak0 <ericjackwong@outlook.com>
2024-07-24 11:49:00 -0400

    set up recipe model

--- models/recipes.js
+++ models/recipes.js
@@ -2,11 +2,49 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Recipe extends Model {
-
+    //nothing special here
 }
 
 Recipe.init(
-
+    //Name / Description / Rating / Instructions OR url / has nuts (or other restrictions)  / Type of food (breakfast, lunch, etc) /
+    {
+        id : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                mine: 1,
+                max: 5
+            }
+        },
+        foodType: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        hasNuts: {
+            //if it isn't inputed, defaults to undefined with is falsey 
+            type: DataTypes.BOOLEAN
+        },
+        gluttenFree: {
+            type: DataTypes.BOOLEAN
+        },
+        vegan: {
+            type: DataTypes.BOOLEAN
+        },
+        url: {
+            //if a url is not given, the link will be created by the id
+            type: DataTypes.STRING
+        }
+    }
 );
 
 module.exports = Recipe;
