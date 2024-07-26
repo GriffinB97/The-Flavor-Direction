d824442ca1e2a2dc4df970696b07e58463df7117
Rovak0 <ericjackwong@outlook.com>
2024-07-26 13:10:15 -0400

    lander works

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.get('/', (req, res) => {
-    res.render('home'); // homepage hanldebar
+    res.render('layouts/main'); // homepage hanldebar
 });
 
 router.use('/api', apiRoutes);
