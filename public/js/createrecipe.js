a16da53d19005f40b6a4c6001eb33a1d5669ccf2
Boss Eggs <wesley.j.locklair@gmail.com>
2024-07-29 13:50:42 -0400

    adding js

--- public/js/createrecipe.js
+++ public/js/createrecipe.js
@@ -3,16 +3,12 @@ const createrecipeFormHandler = async (event) => {
     const title = document.querySelector('#title-recipe').value.trim();
     const description = document.querySelector('#description-recipe').value.trim();
     const instructions = document.querySelector('#instructions-recipe').value.trim();
-    const foodType = document.querySelector('#foodType-recipe').value.trim();
-    // const hasNuts = document.querySelector('#hasnuts-recipe').value.trim();
-    // const glutenFree = document.querySelector('#gluten-recipe').value.trim();
-    // const vegan = document.querySelector('#vegan-recipe').value.trim();
-
+  
     if (title && description && instructions) {
   
       const response = await fetch('/api/users/createrecipe', {
         method: 'POST',
-        body: JSON.stringify({ title, description, instructions, foodType, }),
+        body: JSON.stringify({ title, description, instructions }),
         headers: { 'Content-Type': 'application/json' },
       });
   
